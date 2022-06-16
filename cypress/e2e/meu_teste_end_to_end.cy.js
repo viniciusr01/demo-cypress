c73fbdd27eaa1c350565254e5d88d7b7da76966f
Vinicius Rodrigues <viniciusr02@yahoo.com.br>
2022-06-16 12:01:25 -0300

    Pŕatica Testes End-to-End - Eng Software I

---
+++ cypress/e2e/meu_teste_end_to_end.cy.js
@@ -0,0 +1,63 @@
+describe('Teste End-to-End', () => {
+    it('Teste 1: Visita Página', () => {
+        // abre o site
+        cy.visit('http://localhost:5000/')
+    })
+  })
+
+describe('Teste End-to-End', () => {
+    it('Teste 1: Visita Página', () => {
+        // abre o site
+        cy.visit('http://localhost:5000/')
+    })
+    it('Teste 2: Verifica item na página', () => {
+        // Verifica se existe o livro desejado
+        cy.get('[data-id=3]').should('contain.text', 'Design Patterns')
+    })    
+})
+
+describe('Teste End-to-End', () => {
+    it('Teste 1: Visita Página', () => {
+        // abre o site
+        cy.visit('http://localhost:5000/')
+    })
+    it('Teste 2: Verifica item na página', () => {
+        // Verifica se existe o livro desejado
+        cy.get('[data-id=3]').should('contain.text', 'Design Patterns')
+    })    
+    it('Teste 3: Calcula Frete', () => {    
+        // Calcula o frete
+        cy.get('[data-id=3]').within(() => {
+           cy.get('input').type('10000-000')
+           cy.contains('Calcular Frete').click().then
+           cy.wait(2000)
+        })
+        cy.get('.swal-text').contains('O frete é: R$')
+
+        // Fecha o pop-up com o preço do frete
+        cy.get('.swal-button').click()
+    })
+  })
+
+
+describe('Teste End-to-End - Compra de um Livro', () => {
+    it('Teste 1: Visita Página', () => {
+        // abre o site
+        cy.visit('http://localhost:5000/')
+    })
+    it('Teste 2: Verifica item na página', () => {
+        // Verifica se existe o livro desejado
+        cy.get('[data-id=2]').should('contain.text', 'Engenharia De Software Moderna')
+    })    
+    it('Teste 3: Compra o Livro', () => {    
+        // Compra o Livro Engenharia De Software Moderna
+        cy.get('[data-id=2]').within(() => {
+           cy.contains('Comprar').click().then
+           cy.wait(2000)
+        })
+        cy.get('.swal-text').contains('Sua compra foi realizada com sucesso')
+
+        // Fecha o pop-up com aviso de compra do Livro
+        cy.get('.swal-button').click()
+    })
+})
